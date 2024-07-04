2444d226f8918d33dc3f2f69d95c7af0697deaf7
Anil <iamanilnagvanshi@gmail.com>
2024-07-04 23:58:10 +0530

    Using Declarative API to load the Component Container directly in HTML DOM, instead of loading it from JS.

--- UI_Practice/webapp/index.js
+++ UI_Practice/webapp/index.js
@@ -1,17 +1,19 @@
-sap.ui.define([
-    'sap/ui/core/ComponentContainer'
-], function(ComponentContainer) {
-    'use strict';
-    
-    var oContainer = new ComponentContainer({
-        id: "container",
-        name: "practice",
-        manifest: true,
-        async: true,
-        settings: {
-            id: "practice",
-        }
-    });
+// Commenting the whole file as it is no longer needed. 
 
-    oContainer.placeAt("content");
-});
+// sap.ui.define([
+//     'sap/ui/core/ComponentContainer'
+// ], function(ComponentContainer) {
+//     'use strict';
+    
+//     var oContainer = new ComponentContainer({
+//         id: "container",
+//         name: "practice",
+//         manifest: true,
+//         async: true,
+//         settings: {
+//             id: "practice",
+//         }
+//     });
+
+//     oContainer.placeAt("content");
+// });
