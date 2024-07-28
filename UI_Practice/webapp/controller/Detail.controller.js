5f6fad12b2f289117caadb776ba3102b8b7f3eb7
Anil <iamanilnagvanshi@gmail.com>
2024-07-28 17:07:14 +0530

    Routing back with Hash based navigation implemented.

--- UI_Practice/webapp/controller/Detail.controller.js
+++ UI_Practice/webapp/controller/Detail.controller.js
@@ -1,9 +1,20 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
+], function(Controller, History) {
     'use strict';
     
     return Controller.extend("practice.controller.Detail", {
-
+        onNavBack: function () {
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+          
+            if (sPreviousHash !== undefined) {
+              window.history.go(-1);
+            } else {
+              var oRouter = this.getOwnerComponent().getRouter();
+              oRouter.navTo("overview", {}, true);
+            }
+          }
     });
 });
