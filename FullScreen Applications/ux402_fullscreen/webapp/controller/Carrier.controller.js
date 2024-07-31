7707d00b0588f15378cd752624adf76f35719fea
Anil <iamanilnagvanshi@gmail.com>
2024-08-01 01:44:19 +0530

    Implemented navigation functioanlity with mobile devise responsiveness

--- FullScreen Applications/ux402_fullscreen/webapp/controller/Carrier.controller.js
+++ FullScreen Applications/ux402_fullscreen/webapp/controller/Carrier.controller.js
@@ -1,12 +1,26 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-function (Controller) {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
 
-    return Controller.extend("student27.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.Carrier", {
-        onInit: function () {
+  return Controller.extend(
+    "student27.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.Carrier",
+    {
+      getRouter: function () {
+        return sap.ui.core.UIComponent.getRouterFor(this);
+      },
 
-        }
-    });
+      onPress: function (oEvent) {
+        var oItem = oEvent.getSource();
+        var oCtx = oItem.getBindingContext();
+        var sCarrid = oCtx.getProperty("Carrid");
+
+        this.getRouter().navTo(
+          "flights",
+          {
+            carrid: sCarrid,
+          },
+          false
+        );
+      },
+    }
+  );
 });
--- FullScreen Applications/ux402_fullscreen/webapp/controller/NotFound.controller.js
+++ FullScreen Applications/ux402_fullscreen/webapp/controller/NotFound.controller.js
@@ -1,12 +1,24 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-function (Controller) {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
 
-    return Controller.extend("student27.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.NotFound", {
-        onInit: function () {
+  return Controller.extend(
+    "student27.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.NotFound",
+    {
+      getRouter: function () {
+        return sap.ui.core.UIComponent.getRouterFor(this);
+      },
+      onNavBack: function () {
+        var oHistory, sPreviousHash;
 
+        oHistory = sap.ui.core.routing.History.getInstance();
+        sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          this.getRouter().navTo("overview", true /*no history*/);
         }
-    });
+      },
+    }
+  );
 });
