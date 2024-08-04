f9bed12d97663b2109ee039e51e821451fb8833f
Anil <iamanilnagvanshi@gmail.com>
2024-08-05 02:14:24 +0530

    Implemented all the Logic files including a ListSelector to have sync b/w List and Detail Views

--- List_Detail Applications/ux402_listdetail/webapp/controller/DetailObjectNotFound.controller.js
+++ List_Detail Applications/ux402_listdetail/webapp/controller/DetailObjectNotFound.controller.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "student27/com/sap/training/ux402/listdetail/ux402listdetail/controller/BaseController"
 ],
 function (Controller) {
     "use strict";
