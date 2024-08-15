946f23420eb76d7106f28f31389beefbee67afb5
Anil <iamanilnagvanshi@gmail.com>
2024-08-15 23:03:23 +0530

    Extended a standard Control "sap.m.Button" to have different behaviour on hover

---
+++ FullScreen Applications/ux402_fullscreen/webapp/control/HoverButton.js
@@ -0,0 +1,33 @@
+sap.ui.define(
+  ["sap/m/Button"],
+
+  function (Button) {
+    "use strict";
+
+    return Button.extend(
+      "student27.com.sap.training.ux402.fullscreen.ux402fullscreen.control.HoverButton",
+      {
+        metadata: {
+          properties: {
+            allowHover: {
+              type: "boolean",
+              defaultValue: false,
+            },
+            hoverText: {
+              type: "string",
+            },
+          },
+          events: {
+            hover: {},
+          },
+        },
+        onmouseover: function (evt) {
+          if (this.getAllowHover()) {
+            this.fireHover();
+          }
+        },
+        renderer: {},
+      }
+    );
+  }
+);
--- FullScreen Applications/ux402_fullscreen/webapp/controller/Flights.controller.js
+++ FullScreen Applications/ux402_fullscreen/webapp/controller/Flights.controller.js
@@ -1,54 +1,72 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "student27/com/sap/training/ux402/fullscreen/ux402fullscreen/control/HoverButton",
+    "sap/m/MessageToast"
+  ],
+  function (Controller, HoverButton, MessageToast) {
+    "use strict";
 
-  return Controller.extend(
-    "student27.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.Flights",
-    {
-      onInit: function () {
-        var oRouter = this.getRouter();
-        oRouter.getRoute("flights").attachMatched(this._onObjectMatched, this);
-      },
-      _onObjectMatched: function (oEvent) {
-        var oArgs = oEvent.getParameter("arguments");
-        this._sCarrierId = oArgs.carrid;
-        var oView = this.getView();
-        oView.bindElement({
-          path: "/UX_C_Carrier_TP('" + this._sCarrierId + "')",
-          events: {
-            change: this._onBindingChange.bind(this),
-            dataRequested: function () {
-              oView.setBusy(true);
+    return Controller.extend(
+      "student27.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.Flights",
+      {
+        onInit: function () {
+          var oRouter = this.getRouter();
+          oRouter
+            .getRoute("flights")
+            .attachMatched(this._onObjectMatched, this);
+        },
+        _onObjectMatched: function (oEvent) {
+          var oArgs = oEvent.getParameter("arguments");
+          this._sCarrierId = oArgs.carrid;
+          var oView = this.getView();
+          oView.bindElement({
+            path: "/UX_C_Carrier_TP('" + this._sCarrierId + "')",
+            events: {
+              change: this._onBindingChange.bind(this),
+              dataRequested: function () {
+                oView.setBusy(true);
+              },
+              dataReceived: function () {
+                oView.setBusy(false);
+              },
             },
-            dataReceived: function () {
-              oView.setBusy(false);
-            },
-          },
-        });
-      },
-      _onBindingChange: function () {
-        var oElementBinding;
-        oElementBinding = this.getView().getElementBinding();
+          });
+        },
+        _onBindingChange: function () {
+          var oElementBinding;
+          oElementBinding = this.getView().getElementBinding();
 
-        // No data for the binding
-        if (oElementBinding && !oElementBinding.getBoundContext()) {
-          this.getRouter().getTargets().display("notFound");
-        }
-      },
-      onNavBack: function () {
-        var oHistory, sPreviousHash;
+          // No data for the binding
+          if (oElementBinding && !oElementBinding.getBoundContext()) {
+            this.getRouter().getTargets().display("notFound");
+          }
+        },
+        onNavBack: function () {
+          var oHistory, sPreviousHash;
 
-        oHistory = sap.ui.core.routing.History.getInstance();
-        sPreviousHash = oHistory.getPreviousHash();
+          oHistory = sap.ui.core.routing.History.getInstance();
+          sPreviousHash = oHistory.getPreviousHash();
 
-        if (sPreviousHash !== undefined) {
-          window.history.go(-1);
-        } else {
-          this.getRouter().navTo("overview", true /*no history*/);
-        }
-      },
-      getRouter: function () {
-        return sap.ui.core.UIComponent.getRouterFor(this);
-      },
-    }
-  );
-});
+          if (sPreviousHash !== undefined) {
+            window.history.go(-1);
+          } else {
+            this.getRouter().navTo("overview", true /*no history*/);
+          }
+        },
+        getRouter: function () {
+          return sap.ui.core.UIComponent.getRouterFor(this);
+        },
+
+        onHover: function (evt) {
+          var sText = this.getOwnerComponent()
+            .getModel("i18n")
+            .getProperty("msgSeatsAv");
+          MessageToast.show(evt.getSource().getHoverText() + " " + sText, {
+            duration: 1000,
+          });
+        },
+      }
+    );
+  }
+);
